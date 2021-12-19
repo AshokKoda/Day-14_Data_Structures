753572a4ba9e84b5913a02124d9555274c5df527
Ashok Koda <a.koda59@gmail.com>
2021-12-19 16:01:35 +0530

    Append Linkedlist

--- src/com/bridgelabz/linkedlist/LinkedListProgram.java
+++ src/com/bridgelabz/linkedlist/LinkedListProgram.java
@@ -4,15 +4,20 @@ package com.bridgelabz.linkedlist;
 class Node {
 	int data;
 	Node addressOfNextNode;
+	Node next;
 };
 
 class LinkedList{
 	Node head;
+	Node tail;
 
 	LinkedList() {
 		head = null;
 	}
 	
+	/*
+	 * Add Node
+	 */
 	public void addNode(int newVal) {
 		Node newNode = new Node();
 		newNode.data = newVal;
@@ -28,6 +33,24 @@ class LinkedList{
 		}
 	}
 	
+	/*
+	 * Append Node
+	 */
+	public void appendNode(int newVal) {
+		Node nextNode = new Node();
+		nextNode.data = newVal;
+		if(head == null) {
+			head = nextNode;
+			tail = nextNode;
+		}else {
+			tail.next = nextNode;
+            tail = tail.next;
+		}
+	}
+	
+	/*
+	 * Print list
+	 */
 	public void PrintList() {
 		Node temp = new Node();
 		temp = this.head;
@@ -48,9 +71,9 @@ public class LinkedListProgram {
 	
 	public static void main(String[] args) {
 		LinkedList myList = new LinkedList();
-		myList.addNode(70);
-		myList.addNode(30);
-		myList.addNode(56);
+		myList.appendNode(56);
+		myList.appendNode(30);
+		myList.appendNode(70);
 		myList.PrintList();
 
 	}
