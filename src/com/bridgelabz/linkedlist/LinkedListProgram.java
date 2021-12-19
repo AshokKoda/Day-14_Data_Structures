201d6fb70dae57ba31af7899f4a45da9caf97e83
Ashok Koda <a.koda59@gmail.com>
2021-12-19 15:46:16 +0530

    List ny adding 30 and 56 to 70

--- src/com/bridgelabz/linkedlist/LinkedListProgram.java
+++ src/com/bridgelabz/linkedlist/LinkedListProgram.java
@@ -1,17 +1,57 @@
 package com.bridgelabz.linkedlist;
 
-import java.util.LinkedList;
+
+class Node {
+	int data;
+	Node addressOfNextNode;
+};
+
+class LinkedList{
+	Node head;
+
+	LinkedList() {
+		head = null;
+	}
+	
+	public void addNode(int newVal) {
+		Node newNode = new Node();
+		newNode.data = newVal;
+		newNode.addressOfNextNode = null;
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node temp = new Node();
+			temp = head;
+			while (temp.addressOfNextNode != null)
+				temp = temp.addressOfNextNode;
+			temp.addressOfNextNode = newNode;
+		}
+	}
+	
+	public void PrintList() {
+		Node temp = new Node();
+		temp = this.head;
+		if (temp != null) {
+			System.out.print("Linked List have: ");
+			while (temp != null) {
+				System.out.print(temp.data + " ");
+				temp = temp.addressOfNextNode;
+			}
+			System.out.println();
+		} else {
+			System.out.println("The list is empty.");
+		}
+	}
+}
 
 public class LinkedListProgram {
-
+	
 	public static void main(String[] args) {
-		
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		list.add(56);
-		list.add(30);
-		list.add(76);
-		
-		System.out.println("LinkedList is : " + list);
+		LinkedList myList = new LinkedList();
+		myList.addNode(70);
+		myList.addNode(30);
+		myList.addNode(56);
+		myList.PrintList();
 
 	}
 
