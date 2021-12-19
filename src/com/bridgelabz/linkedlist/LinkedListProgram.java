a60035816b13c698aee7aa39439ea46205cedabd
Ashok Koda <a.koda59@gmail.com>
2021-12-19 15:30:07 +0530

    Create simple linkedlist

---
+++ src/com/bridgelabz/linkedlist/LinkedListProgram.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.linkedlist;
+
+import java.util.LinkedList;
+
+public class LinkedListProgram {
+
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(56);
+		list.add(30);
+		list.add(76);
+		
+		System.out.println("LinkedList is : " + list);
+
+	}
+
+}
